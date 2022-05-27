37b3ffe091a1ace340ed99c3939d52310035b155
ilanius <leonard.ilanius@gmail.com>
2022-05-27 21:12:51 +0200

    edit

--- demo.js
+++ demo.js
@@ -3,26 +3,10 @@
 /* ***************************************** */
 
 const fs = require('fs')
-var data;
-var data = fs.readFileSync('input.txt', 'utf8')
-
-var input = data.split( /[ \r\n]/);
-
-/* In theory the functions A to F and their conditionals can be quite complex */
-function A( p ) { }
-var [B,C,D,E,F] = [A,A,A,A,A];  
-var funcArray = [
-    { 'condition': /^A/,  'func':A, 'stat':0  },
-    { 'condition': /^B/,  'func':B, 'stat':0  },
-    { 'condition': /^C/,  'func':C, 'stat':0  },
-    { 'condition': /^D/i, 'func':D, 'stat':0  },
-    { 'condition': /^E/i, 'func':E, 'stat':0  },
-    { 'condition': /^F/,  'func':F, 'stat':0  },
-];
 
 function processInput( headerTxt, input, funcArray ) {
     console.log( headerTxt );
-    var tme = Date.now();
+    var time = Date.now();
     for ( var i in input ) {
         var itm = input[i];
         if ( itm == '/*') cont = true;
@@ -40,18 +24,32 @@ function processInput( headerTxt, input, funcArray ) {
         }
         if ( j == funcArray.length ) console.log( '==>No match for "' + itm + '" at position: ' + i );
     }
-    console.log('Time elapsed:' + ( Date.now() - tme ) );
+    console.log('Time elapsed:' + ( Date.now() - time ) );
     console.log( 'The funcArray after processing' );
     console.log( funcArray );
     console.log(  );
-
 }
-var tme;
-tme = processInput( 'Array of conditions. Unsorted conditions.',  input, funcArray );
+
+var data = fs.readFileSync('input.txt', 'utf8').split(/[ \r\n]/ );
+
+/* The functions A to F and their conditionals may be quite complex */
+function A( p ) { }
+var [B,C,D,E,F] = [A,A,A,A,A];  
+var funcArray = [
+    { 'condition': /^A/,  'func':A, 'stat':0  },
+    { 'condition': /^B/,  'func':B, 'stat':0  },
+    { 'condition': /^C/,  'func':C, 'stat':0  },
+    { 'condition': /^D/i, 'func':D, 'stat':0  },
+    { 'condition': /^E/i, 'func':E, 'stat':0  },
+    { 'condition': /^F/,  'func':F, 'stat':0  },
+];
+
+var time;
+time = processInput( 'Array of conditions. Unsorted conditions.',  data, funcArray );
 
 funcArray.sort( (a,b) => b.stat - a.stat ); /* most called functions come first */
-tme = processInput( 'Array of conditions. Conditions sorted for maximum speed.', input, funcArray );
+time = processInput( 'Array of conditions. Conditions sorted for maximum speed.', data, funcArray );
 
 funcArray.sort( (a,b) => a.stat - b.stat ); /* most called functions come last */
-tme = processInput( 'Array of conditions. Conditions sorted for minimum speed.', input, funcArray );
+time = processInput( 'Array of conditions. Conditions sorted for minimum speed.', data, funcArray );
 
